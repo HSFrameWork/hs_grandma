65c5f05c81e386971ede0a6494b5af490165d68f
HS FrameWork <133069863+HSFrameWork@users.noreply.github.com>
2023-05-11 06:30:12 +0530

    Add files via upload

---
+++ hs_grandma/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Original Author: HS FrameWork'
+version '1'
+lua54 'yes'
+
+shared_scripts {
+    'config.lua',
+}
+client_scripts {
+    'client/*.lua'
+}
+server_scripts {
+    'server/*.lua'
+}
